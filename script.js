095f22e3c67843448a4bf6cdfa192d1bd36e7311
emi <ejhartline@gmail.com>
2020-03-22 18:57:35 -0400

    html and css updates

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-// Brewery DB query
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries?by_state=NY&by_name=cooper&by_tag=patio&by_type=micro",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "brianiswu-open-brewery-db-v1.p.rapidapi.com",
-		"x-rapidapi-key": "ad79893db0msh519507ce219a2cep1942b9jsn16290fa29320"
-	}
-};
-
-=======
 // var userLat = 37.540726;
 // var userLon = -77.436050;
 
@@ -53,7 +39,6 @@ var settings = {
 		"x-rapidapi-key": "cdc0ac54bamshcdd268cd5c832d8p100753jsn8ab4fd9912f8"
 	}
 }
->>>>>>> c6a80fbe0d6445e03e36e5a3132a1480af2dbcc9
 $.ajax(settings).done(function (response) {
 	console.log(response);
 });
