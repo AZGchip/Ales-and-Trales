ea2ddc1b5c6393cce31e9e5dff6c434ed662da77
Daniel Abell <da450@cairn.edu>
2020-03-23 14:09:22 -0400

    added <form> to html to make 'enter' trigger click event. updated js file

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+var searchbox;
+let searchLat;
+let searchlon;
+
+$("#user-input").on("click", function(event) {
+    event.preventDefault();
+    searchbox = $(this).prev().val();
+    console.log(searchbox);
+    //api value 
+    var apiUrl =  "https://nominatim.openstreetmap.org/search/"+ searchbox + "?format=json";
+    var results;
+    function getLatLon(){
+        $.ajax({
+            url: apiUrl,
+            method: "GET"
+        })
+            .then(function (response) {
+                results = response;
+                searchLat = results[0].lat
+                searchlon = results[0].lon
+                console.log(results)
+            });
+    };
+    getLatLon();
+});
+
 // var userLat = 37.540726;
 // var userLon = -77.436050;
 
@@ -29,16 +55,19 @@
 //     console.log(response);
 // }
 
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/search?query=richmond",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "brianiswu-open-brewery-db-v1.p.rapidapi.com",
-		"x-rapidapi-key": "cdc0ac54bamshcdd268cd5c832d8p100753jsn8ab4fd9912f8"
-	}
-}
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+// var settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/search?query=richmond",
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "brianiswu-open-brewery-db-v1.p.rapidapi.com",
+// 		"x-rapidapi-key": "cdc0ac54bamshcdd268cd5c832d8p100753jsn8ab4fd9912f8"
+// 	}
+// }
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
+
+
+
